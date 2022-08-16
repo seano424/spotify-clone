3492f979567ff3ae0e1229849c425e2b002567c6
Sean OReilly <soreilly424@gmail.com>
2022-08-16 16:13:22 -0500

    Added Player UI

--- components/Player.tsx
+++ components/Player.tsx
@@ -83,6 +83,25 @@ const Player = () => {
           <Box width="10%">
             <Text fontSize="xs">1:21</Text>
           </Box>
+          <Box width="80%">
+            <RangeSlider
+              aria-label={['min', 'max']}
+              step={0.1}
+              min={0}
+              max={300}
+              id="player-range"
+            >
+              <RangeSliderTrack bg="gray.800">
+                <RangeSliderFilledTrack bg="gray.600" />
+              </RangeSliderTrack>
+              <RangeSliderThumb index={0} />
+            </RangeSlider>
+          </Box>
+          <Box width="10%">
+            <Text fontSize="xs" textAlign="right">
+              3:21
+            </Text>
+          </Box>
         </Flex>
       </Box>
     </Box>
